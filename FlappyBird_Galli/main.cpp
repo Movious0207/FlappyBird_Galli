9545be4ae109c86641384fd34b5b8927d5b3285c
Lucas Galli <luquigalli0207@gmail.com>
2025-10-31 11:06:14 -0300

    added player and obstacle

--- FlappyBird_Galli/main.cpp
+++ FlappyBird_Galli/main.cpp
@@ -10,18 +10,34 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    Vector2 playerPos = { 0 };
+    Vector2 obstaclePos = { 0 };
 
-    SetTargetFPS(60);   
+
+    InitWindow(screenWidth, screenHeight, "Flappy Bird");
  
+
     while (!WindowShouldClose())    
     {
+
+        playerPos.x = 200;
+
+        if (IsKeyDown('w')|| IsKeyDown('W'))
+        {
+
+        }
+        else if (IsKeyDown('s') || IsKeyDown('S'))
+        {
+
+        }
 
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
+        DrawRectangleV(playerPos, { 40,40 }, BLACK);
 
+        DrawRectangleV(obstaclePos, { 40,screenHeight }, BLACK);
 
         EndDrawing();
     }
